e73ea11a989c070e06bce3d9b48e7ef2b7ac2c50
parthbsawant <parthbsawant@gmail.com>
2025-11-19 23:13:27 +0530

    Fix PORT for Azure deployment

--- backend/server.js
+++ backend/server.js
@@ -113,7 +113,8 @@ app.post('/api/log-download', async (req, res) => {
 });
 
 // Start server
-const PORT = 4000;
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
